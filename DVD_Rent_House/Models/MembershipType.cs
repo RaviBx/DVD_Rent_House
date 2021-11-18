46551ff4e096a9c95b5f414d29a6cf0d1cfa583b
Ravishan Balasooriya <ravimuay@gmail.com>
2021-11-18 15:36:24 +0530

    Added MembershipType class

---
+++ DVD_Rent_House/Models/MembershipType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DVD_Rent_House.Models
+{
+    public class MembershipType
+    {
+        public byte Id { get; set; }
+        public short SignUpFee { get; set; }
+        public byte DurationInMonths { get; set; }
+        public byte DiscountRate { get; set; }
+
+
+
+    }
+}
