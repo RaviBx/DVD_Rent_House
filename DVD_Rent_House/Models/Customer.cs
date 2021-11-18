e298d95a55abdbefbc74d103aa81d47b21956cae
Ravishan Balasooriya <ravimuay@gmail.com>
2021-11-18 16:36:16 +0530

    Updated Customer Name

--- DVD_Rent_House/Models/Customer.cs
+++ DVD_Rent_House/Models/Customer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -10,6 +11,8 @@ namespace DVD_Rent_House.Models
 
         public int Id { get; set; }
 
+        [Required]
+        [StringLength(255)]
         public string Name { get; set; }
 
         public bool IsSubcribedToNewsLetter { get; set; }
